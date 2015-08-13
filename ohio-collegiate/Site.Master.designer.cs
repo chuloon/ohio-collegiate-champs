c512ae9d1b68e8b114acd71cbe5068abb7959d77
chuloon <ben.ng@outlook.com>
2015-08-12 23:43:12 -0400

    new nav menu

--- ohio-collegiate/Site.Master.designer.cs
+++ ohio-collegiate/Site.Master.designer.cs
@@ -22,6 +22,15 @@ namespace ohio_collegiate {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
         
         /// <summary>
+        /// A3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor A3;
+        
+        /// <summary>
         /// FeaturedContent control.
         /// </summary>
         /// <remarks>
